f4d0fc05ad9965d9baef1c4892763df8403d21d1
starryjay <sshree@usc.edu>
2024-07-08 20:56:36 -0700

    Updated review scraper

--- ReviewScraper.py
+++ ReviewScraper.py
@@ -5,12 +5,23 @@ from pathlib import Path
 import steamspypi
 import steamreviews
 import pandas as pd
+
 def download_reviews(page_no=1):
     print(f"Downloading reviews for page={page_no} on {time.asctime()}")
+    with open(f'20240706_steamspy_page_{page_no}.json') as f:
+        page = json.load(f)
+    app_ids = page.keys()
+    reviews = steamreviews.download_reviews_for_app_id_batch(app_ids)
+    return type(reviews)
+
+if __name__ == '__main__':
+    for pagenum in range(64):
+        download_reviews(pagenum)
+    
 
     # For every page in folder
-    # For every game id in page
-    # Call steam_review_scraper.get_game_review(id=game_id)
+    # Get all game ids
+    # Call steamreviews.download_reviews_for_app_id_batch(app_ids)
     # Store result in table
     # Concatenate tables until end of page
     # Output to csv and start over
